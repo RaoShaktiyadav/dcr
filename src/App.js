5221694d68e3c1a6a8140bd473f636a7d3464528
shakti <shaktiyadav635@gmail.com>
2024-09-13 10:23:07 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./component/img/logo.jpg";
+import "./App.css";
+import "./component/Contact.css";
+import "./component/Home.css";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+  } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from "./component/Home";
+import Cars from "./component/Cars";
 
+import About from "./component/About";
+import Contact from "./component/Contact";
+
+
+
+
+
+    
+
+    
+const  App =() => {
+  // { onSearch }
+  //   const [searchTerm, setSearchTerm] = useState("");
+  
+  //   const handleInputChange = (e) => {
+  //     setSearchTerm(e.target.value);
+  //   };
+  
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     onSearch(searchTerm);
+  //   };  
+  
+  // const handleSearch = (query) => {
+  //   console.log("Searching for:", query);
+  //   // Implement the search logic here, e.g., filter data, make API calls, etc.
+  // };
+
+
+
+    return (
+            <Router>
+                <div>
+        <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light" >
+        <div class="container-fluid">
+         <img  src={logo} class="navbar-brand" to="/" alt="img"/> 
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/Cars">Cars</Link>
+              </li>
+              
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/About">About</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/Contact">Contact</Link>
+              </li>
+             
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2"  placeholder="Search..."/>
+              <button class="btn btn-outline-success" type="submit" >Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+
+
+    
+      
+                   
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Home />}
+                        ></Route>
+                        <Route
+                            path="/cars"
+                            element={<Cars/>}
+                        ></Route>
+                        {/* <Route
+                            path="/Blog"
+                            element={<Blog />}
+                        ></Route> */}
+                        <Route
+                            path="/about"
+                            element={<About />}
+                        ></Route>
+                        <Route
+                            path="/contact"
+                            element={<Contact />}
+                        ></Route>
+                    </Routes>
+                </div>
+            </Router>
+        );
+    
+    };
+  
 export default App;
+
---
+++ src/component/Home.js
@@ -0,0 +1,380 @@
+import React from "react";
+
+
+
+import log from './img/logo.jpg'
+import g1 from './img/f1.png'
+import g2 from './img/f2.png'
+import g3 from './img/f3.png'
+import g4 from './img/f4.png'
+import g5 from './img/f5.png'
+import g6 from './img/f6.png'
+
+import f1 from './img/black s11.jpg'
+import f2 from './img/white s11.jpg'
+import f3 from './img/black classic s11.jpg'
+import f4 from './img/Mahindra-Thar-.jpg'
+import f5 from './img/swift.jpg'
+import f6 from './img/baleno.jpg'
+import f7 from './img/verna.jpg'
+import f8 from './img/i20.jpg'
+
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const  Home = ({ phoneNumber, message }) => {
+  const navigate =useNavigate();
+
+  const fncar= () =>{
+    navigate('/cars');
+  };
+
+  
+    const whatapp = () => {
+      const encodedMessage = encodeURIComponent("Hi Sir good evining ");
+      const whatsappLink = `https://wa.me/${+918901636636}?text=${encodedMessage}`;
+      window.open(whatsappLink, '_blank');
+    };
+    const what15 = () => {
+      const encodedMessage = encodeURIComponent("Hi Sir good evining we picup a car for one month and we get offer of 15% off");
+      const whatsappLink = `https://wa.me/${+918901636636}?text=${encodedMessage}`;
+      window.open(whatsappLink, '_blank');
+    };
+    const what10 = () => {
+      const encodedMessage = encodeURIComponent("Hi Sir good evining we picup a car for ten days and we get offer of 10% off ");
+      const whatsappLink = `https://wa.me/${+918901636636}?text=${encodedMessage}`;
+      window.open(whatsappLink, '_blank');
+    };
+
+    const [inputValue, setInputValue] = useState('');
+
+    // Handle changes in the input field
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
+  
+    // Store the input value in localStorage on button click without erasing previous data
+    const saveData = () => {
+      // Get the existing data from localStorage
+      const existingData = JSON.parse(localStorage.getItem('myInputData')) || [];
+      
+      // Add the new input value to the array
+      const updatedData = [...existingData, inputValue];
+  
+      // Store the updated array back in localStorage
+      localStorage.setItem('myInputData', JSON.stringify(updatedData));
+  
+      // Clear the input field
+      setInputValue('');
+  
+     
+    };
+    const loction = () => {
+      // some complex logic here
+      window.open("https://maps.app.goo.gl/wLvT7pK76kB3se4z6","_blank")
+    };
+    const insta = () => {
+      // some complex logic here
+      window.open("https://www.instagram.com/shree_shyam_car_rental/?igsh=MXE0a2JkNzBqamhyNw%3D%3D","_blank")
+    };
+
+    const Dail = () => {
+      const phoneNumber = "+918901636636";
+      if (navigator.userAgent.match(/Mobi/)) {
+        // It's a mobile device
+        window.location.href = `tel:${phoneNumber}`;
+      } else {
+        alert("This function is only available on mobile devices.");
+      }
+    };
+
+
+    const mail = () => {
+      const email = "shaktiyadav635@gmail.com";
+      const subject = "Hello from Rental";
+      const body = "I wanted to reach out to you regarding car details or somthing else ...";
+      const cc = "cc@example.com";
+      const bcc = "bcc@example.com";
+      
+      window.location.href = `mailto:${email}?cc=${encodeURIComponent(cc)}&bcc=${encodeURIComponent(bcc)}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    };
+    
+
+    return (
+     
+
+
+
+
+
+    <div className="hback">
+     <section id="hero">
+        <h4>Cars For Rental</h4>
+        <h2>Affordable price</h2>
+        <h1> All Cars Available</h1>
+        <p>save more coupons &amp; up to 5% off!</p>
+        <button onClick={fncar}>Book now</button>
+      </section>
+      
+
+
+      <section id="features" class="section-p1">
+        <div class="f-box">
+          <img src={g1} alt="free shipping"/>
+          <h6>Home Pickup</h6>
+        </div>
+        <div class="f-box">
+          <img src={g2} alt="online order"/>
+          <h6>Online Booking</h6>
+        </div>
+        <div class="f-box thr">
+          <img src={g3} alt="save money"/>
+          <h6>Save money</h6>
+        </div>
+       
+        <div class="f-box  ">
+          <img src={g4} alt="promotions"/>
+          <h6>Promotions</h6>
+        </div>
+        <div class="f-box">
+          <img src={g5} alt="happy sell"/>
+          <h6>Happy Coustmer</h6>
+        </div>
+        <div class="f-box">
+          <img src={g6} alt="24/7 support"/>
+          <h6>24/7 Support</h6>
+        </div>
+      </section>
+
+
+      <section className="product-section">
+        <h1 className="h1">Available Cars</h1>
+        <p>All Types Cars Top Model</p>
+        <div className="pro-collection">
+          <div className="product-cart">
+            <img src={f1} alt="product img"/>
+            <span>Mahindra</span>
+            <h4>Black Scorpio S11</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹5500</h4>
+            <i onClick={whatapp} class="fa-brands fa-square-whatsapp fa-beat"></i>
+            </div>
+          <div class="product-cart">
+            <img src={f2} alt="product img"/>
+            <span>Mahindra</span>
+            <h4>White Scorpio S11</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹5500</h4>
+            <i onClick={whatapp}  class="fa-brands fa-square-whatsapp fa-beat"></i>
+          </div>
+          <div class="product-cart">
+            <img src={f3} alt="product img"/>
+            <span>Mahindra</span>
+            <h4>Black classic Scorpio S11</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹5500</h4>
+            <i  onClick={whatapp} class="fa-brands fa-square-whatsapp fa-beat"></i>
+            </div>
+          <div class="product-cart">
+            <img src={f4} alt="product imag"/>
+            <span>Mahindra</span>
+            <h4>Black Thar 4*4</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹6000</h4>
+            <i onClick={whatapp}  class="fa-brands fa-square-whatsapp fa-beat"></i>
+            </div>
+          <div class="product-cart">
+            <img src={f5} alt="product img"/>
+            <span>Maruti Suzuki</span>
+            <h4> White Swift</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹2500</h4>
+            <i onClick={whatapp}  class="fa-brands fa-square-whatsapp fa-beat"></i>
+            </div>
+          <div class="product-cart">
+            <img src={f6} alt="product img"/>
+            <span>Maruti Suzuki</span>
+            <h4>Silver Baleno</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹3000</h4>
+            <i onClick={whatapp}  class="fa-brands fa-square-whatsapp fa-beat"></i>
+            </div>
+          <div class="product-cart">
+            <img src={f7} alt="product img"/>
+            <span>Hundai</span>
+            <h4>White Verna 1.6</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹4800</h4>
+            <i onClick={whatapp}  class="fa-brands fa-square-whatsapp fa-beat"></i>
+            </div>
+          <div class="product-cart">
+            <img src={f8} alt="product img"/>
+            <span>Hundai</span>
+            <h4>White i20 Asta</h4>
+            <div class="stars">
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+              <i class="fa-solid fa-star" aria-hidden="true"></i>
+            </div>
+            <h4 class="price">₹3000</h4>
+            <i onClick={whatapp}  class="fa-brands fa-square-whatsapp fa-beat"></i>
+             </div>
+          
+        </div>
+        <button className="butt" onClick={fncar}>Show More</button>
+      </section>
+
+   
+
+
+      <section id="banners" class="section-p1">
+        <div class="big-banners">
+          <div class="big-banners-1">
+            <h4>Crazy Deals</h4>
+            <h2>Get 15% off on one month Booking</h2>
+            <span>Like new condition cars on rent</span>
+            <button class="banner-btn" onClick={what15}>Check here</button>
+          </div>
+          <div class="big-banners-2">
+            <h4>Daily Deals</h4>
+            <h2>Get 10% off on 10 days Booking</h2>
+            <span>Clean and hygienic</span>
+            <button class="banner-btn" onClick={what10}>Collection</button>
+          </div>
+        </div>
+        <div class="small-banners">
+          <div class="small-banners-1">
+            <h2 className="a">FOR MORE OFFERS</h2>
+            <h5>Check daily our website</h5>
+          </div>
+          <div class="small-banners-2" onClick={insta}>
+            <h2  className="a">FOLLOW US ON SOCIAL MEDIA</h2>
+            <h5 >Instagram</h5>
+          </div>
+          <div class="small-banners-3">
+            <h2  className="a">CARS FOR RENT</h2>
+            
+          </div>
+        </div>
+      </section>
+
+
+
+      <footer className="footer-section">
+    <div className="container">
+        <div className="footer-cta pt-5 pb-5">
+            <div className="row">
+                <div className="col-xl-4 col-md-4 mb-30">
+                    <div className="single-cta">
+                        <i className="fas fa-map-marker-alt"></i>
+                        <div className="cta-text" onClick={loction}>
+                            <h4 className="footex">Find us</h4>
+                            <span className="footex">Near Manipal University jaipur</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xl-4 col-md-4 mb-30">
+                    <div className="single-cta">
+                        <i className="fas fa-phone"></i>
+                        <div className="cta-text" onClick={Dail}>
+                            <h4 className="footex">Call us</h4>
+                            <span>8901636636</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xl-4 col-md-4 mb-30">
+                    <div className="single-cta">
+                        <i className="far fa-envelope-open"></i>
+                        <div className="cta-text" onClick={mail}>
+                            <h4 className="footex">Mail us</h4>
+                            <span className="footex">shaktiyadav635@gmail.com</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="footer-content pt-5 pb-5">
+            <div className="row">
+                <div className="col-xl-4 col-lg-4 mb-50">
+                    <div className="footer-widget">
+                        <div className="footer-logo">
+                            <img src={log} className="img-fluid" alt="logo"/>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                    <div className="footer-widget">
+                        <div className="footer-widget-heading">
+                            <h3 className="footex">Subscribe</h3>
+                        </div>
+                        <div className="footer-text mb-25">
+                            <p className="footex">Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                        </div>
+                        <div className="subscribe-form">
+                            <form action="#" class="ng-pristine ng-valid">
+                                <input className="inco" type="email" value={inputValue} onChange={handleInputChange} placeholder="Email Address"/>
+                                <button id="but" onClick={saveData}><i className="fab fa-telegram-plane"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button id="button" onClick={whatapp} ><i class="fa-brands fa-whatsapp ba1" ></i></button>
+    
+    
+</footer>
+      
+      
+      </div>
+    )
+}
+
+export default Home;
