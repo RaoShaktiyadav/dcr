126a76c32fbf1fc8f4a4f3e55065788bb618c398
shakti <shaktiyadav635@gmail.com>
2024-09-25 06:11:04 +0530

    s

--- src/component/Home.js
+++ src/component/Home.js
@@ -118,34 +118,7 @@ const  Home = ({ phoneNumber, message }) => {
       
 
 
-      <section id="features" class="section-p1">
-       
-        <div class="f-box">
-          <img src={g1} alt="free shipping"/>
-          <h6>Home Pickup</h6>
-        </div>
-        <div class="f-box">
-          <img src={g2} alt="online order"/>
-          <h6>Online Booking</h6>
-        </div>
-        <div class="f-box thr">
-          <img src={g3} alt="save money"/>
-          <h6>Save money</h6>
-        </div>
-       
-        <div class="f-box  ">
-          <img src={g4} alt="promotions"/>
-          <h6>Promotions</h6>
-        </div>
-        <div class="f-box">
-          <img src={g5} alt="happy sell"/>
-          <h6>Happy Coustmer</h6>
-        </div>
-        <div class="f-box">
-          <img src={g6} alt="24/7 support"/>
-          <h6>24/7 Support</h6>
-        </div>
-      </section>
+      
 
 
       <section className="product-section">
@@ -303,7 +276,34 @@ const  Home = ({ phoneNumber, message }) => {
         </div>
       </section>
 
-
+      <section id="features" class="section-p1">
+       
+        <div class="f-box">
+          <img src={g1} alt="free shipping"/>
+          <h6>Home Pickup</h6>
+        </div>
+        <div class="f-box">
+          <img src={g2} alt="online order"/>
+          <h6>Online Booking</h6>
+        </div>
+        <div class="f-box thr">
+          <img src={g3} alt="save money"/>
+          <h6>Save money</h6>
+        </div>
+       
+        <div class="f-box  ">
+          <img src={g4} alt="promotions"/>
+          <h6>Promotions</h6>
+        </div>
+        <div class="f-box">
+          <img src={g5} alt="happy sell"/>
+          <h6>Happy Coustmer</h6>
+        </div>
+        <div class="f-box">
+          <img src={g6} alt="24/7 support"/>
+          <h6>24/7 Support</h6>
+        </div>
+      </section>
 
       <footer className="footer-section">
     <div className="container">
